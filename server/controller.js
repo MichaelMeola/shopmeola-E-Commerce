4858c0e607d5ce00438b22fa72a691dff103164b
Michael Meola <meolaadonai@gmail.com>
2023-12-20 11:54:02 -0700

    finished crud routes on backend

--- server/controller.js
+++ server/controller.js
@@ -2,10 +2,38 @@ import { Product } from './db/models.js'
 
 const handlerFunctions = {
 
-    getProducts: async (req, res) => {
+    getProducts: async(req, res) => {
         const allProducts = await Product.findAll()
         res.send(allProducts)
-    }
+    },
+
+    addProduct: async (req, res) => {
+        await Product.create({})
+        const allProducts = await Product.findAll()
+        res.send(allProducts)
+      },
+
+      deleteProduct: async (req, res) => {
+        const { id } = req.params
+      
+        await Product.destroy({ where: { id } })
+      
+        const allProducts = await Product.findAll()
+        res.send(allProducts)
+      },
+
+    editProduct: async (req, res) => {
+        const { id } = req.params
+        const { name, description, price, s, m, l, xl, image } = req.body
+      
+        await Product.update(
+          { name, description, price, s, m, l, xl, image },
+          { where: { id } }
+        )
+      
+        const allProducts = await Product.findAll()
+        res.send(allProducts)
+      }
 }
 
 export default handlerFunctions
--- server/index.js
+++ server/index.js
@@ -10,8 +10,13 @@ app.use(express.urlencoded({extended: false}))
 app.use(morgan('dev'))
 
 import handlerFunctions from "./controller.js"
-const { getProducts } = handlerFunctions
+
+const { getProducts, addProduct, deleteProduct, editProduct } = handlerFunctions
 
 app.get('/products', getProducts)
+app.post('/product', addProduct)
+app.delete('/product/:id', deleteProduct)
+app.put('/product/:id', editProduct)
+
 
 ViteExpress.listen(app, 6969, () => console.log(`Server running on http://localhost:6969`))
